9794466a821a1b1ebe9d2bd9a6101793f54d17c2
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-02-23 12:48:39 -0800

    add create user dto

--- http-api-gateway/src/users/dtos/create-user.dto.ts
+++ http-api-gateway/src/users/dtos/create-user.dto.ts
@@ -1,12 +1,23 @@
-import { IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty()
   @IsString()
+  @MaxLength(32)
   username: string;
 
+  @IsOptional()
   @IsString()
+  @MaxLength(64)
   displayName?: string;
 
-  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 }
