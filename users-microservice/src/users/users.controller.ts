63897d59f05bd6c8e60b5869ca182958bb9a7d93
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-02-29 09:56:36 -0800

    add controller to user microservice

--- users-microservice/src/users/users.controller.ts
+++ users-microservice/src/users/users.controller.ts
@@ -1,11 +1,9 @@
-import { Controller } from "@nestjs/common";
-import { MessagePattern, Payload } from "@nestjs/microservices";
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller()
 export class UsersMicroserviceController {
-  
-  @MessagePattern({cmd: 'createUser'})
-  createUser(@Payload() data:) {
-
-  }
+  @MessagePattern({ cmd: 'createUser' })
+  createUser(@Payload() data: CreateUserDto) {}
 }
